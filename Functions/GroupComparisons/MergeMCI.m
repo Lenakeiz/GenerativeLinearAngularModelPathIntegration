fbc3e22bfe2f2b29fc9371bab45e80e8f7ab3841
ZIlong Ji <jzlxmu@163.com>
2022-02-17 17:01:00 +0000

    relocated the folder
    relocated the folder

--- Functions/GroupComparisons/MergeMCI.m
+++
@@ -1,7 +0,0 @@
-function MCI = MergeMCI(MCIPos, MCINeg, MCIUnk)
-    numConds = size(MCIPos,2);
-    MCI = cell(1, numConds);
-    for condidx =1:numConds
-        MCI{condidx} = [MCIPos{condidx};MCINeg{condidx};MCIUnk{condidx}];
-    end
-end

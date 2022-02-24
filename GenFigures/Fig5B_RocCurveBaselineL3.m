79f3590b2d3a164f84bcddb8595b2b9551d20447
Lenakeiz_UCL <andrea.castegnaro@outlook.com>
2022-02-24 16:57:38 +0000

    Added difference between theta3 and theta3hat to calculate the roc curve

--- GenFigures/Fig5B_RocCurveBaselineL3.m
+++ GenFigures/Fig5B_RocCurveBaselineL3.m
@@ -33,17 +33,16 @@ config.ModelName = "BaseModel";
 config.NumParams = 5;
 
 % Model fitting for HealthyOld data
-[~, AllHealthyOldX, AllHealthyOldDX, ~, ~] = getResultsAllConditions(HealthyControls, config);
-[~, AllMCIAgg, AllMCIAggDX, ~, ~]          = getResultsAllConditions(MCIAll, config);
-[~, AllMCINegX, AllMCINegDX, ~, ~]         = getResultsAllConditions(MCINeg, config);
-[~, AllMCIPosX, AllMCIPosDX, ~, ~]         = getResultsAllConditions(MCIPos, config);
+[~, AllHealthyOldX, AllHealthyOldDX, AllHealthyOldTheta, ~] = getResultsAllConditions(HealthyControls, config);
+[~, AllMCIAgg, AllMCIAggDX, AllMCIAggTheta, ~]              = getResultsAllConditions(MCIAll, config);
+[~, AllMCINegX, AllMCINegDX, AllMCINegTheta, ~]             = getResultsAllConditions(MCINeg, config);
+[~, AllMCIPosX, AllMCIPosDX, AllMCIPosTheta, ~]             = getResultsAllConditions(MCIPos, config);
 
-%% Calculating l3 - l3 hat (walked length - real return length)
-
-[L3_L3Hat_HC]     = getReturnAndActualDistance(AllHealthyOldX, AllHealthyOldDX);
-[L3_L3Hat_MCIAll] = getReturnAndActualDistance(AllMCIAgg, AllMCIAggDX);
-[L3_L3Hat_MCINeg] = getReturnAndActualDistance(AllMCINegX, AllMCINegDX);
-[L3_L3Hat_MCIPos] = getReturnAndActualDistance(AllMCIPosX, AllMCIPosDX);
+%% Calculating l3 - l3 hat (walked length - real return length) and calculating Theta3 - Theta3Hat
+[L3_L3Hat_HC, Theta3_Theta3Hat_HC]         = getReturnAndActualDistance(AllHealthyOldX, AllHealthyOldDX, AllHealthyOldTheta);
+[L3_L3Hat_MCIAll, Theta3_Theta3Hat_MCIAll] = getReturnAndActualDistance(AllMCIAgg, AllMCIAggDX, AllMCIAggTheta);
+[L3_L3Hat_MCINeg, Theta3_Theta3Hat_MCINeg] = getReturnAndActualDistance(AllMCINegX, AllMCINegDX, AllMCINegTheta);
+[L3_L3Hat_MCIPos, Theta3_Theta3Hat_MCIPos] = getReturnAndActualDistance(AllMCIPosX, AllMCIPosDX, AllMCIPosTheta);
 
 %% Creating the roc curves for MCI all vs HC
 close all;
@@ -61,8 +60,11 @@ set(0,'DefaultTextFontSize',12)
 
 hold on;
 
-AUC = plotROCCurve(L3_L3Hat_HC, L3_L3Hat_MCIAll, {'L3_L3Hat'}, 'L3_L3Hat_MCIvsHC','HC', 'MCI', config.color_scheme_npg(4,:), config);
-legendText = ['AUC(l3-$\hat{l3}$) = ' , num2str(round(AUC.Value(1),2),2)];
+AUC{1} = plotROCCurve(L3_L3Hat_HC, L3_L3Hat_MCIAll, {'L3_L3Hat'}, 'L3_L3Hat_MCIvsHC','HC', 'MCI', config.color_scheme_npg(4,:), config);
+legendText{1,1} = ['AUC(l3-$\hat{l3}$) = ' , num2str(round(AUC{1}.Value(1),2),2)];
+
+AUC{2} = plotROCCurve(Theta3_Theta3Hat_HC, Theta3_Theta3Hat_MCIAll, {'Theta3_Theta3Hat'}, 'Theta3_Theta3Hat_MCIvsHC','HC', 'MCI', config.color_scheme_npg(8,:), config);
+legendText{1,2} = ['AUC($\theta$3-$\hat{\theta 3}$) = ' , num2str(round(AUC{2}.Value(1),2),2)];
 
 hold off;
     
@@ -85,8 +87,8 @@ set(gca, ...
 
 axis square;
 
-exportgraphics(f,config.ResultFolder+"/ROCL3_L3Hat_MCIvsHC"+".png",'Resolution',300);
-exportgraphics(f,config.ResultFolder+"/ROCL3_L3Hat_MCIvsHC"+".pdf",'Resolution',300, 'ContentType','vector');
+exportgraphics(f,config.ResultFolder+"/ROC_ActualVsReal_MCIvsHC"+".png",'Resolution',300);
+exportgraphics(f,config.ResultFolder+"/ROC_ActualVsReal_MCIvsHC"+".pdf",'Resolution',300, 'ContentType','vector');
 
 clear parametersName filesName i colors f ll legendText 
 
@@ -108,8 +110,11 @@ set(0,'DefaultTextFontSize',12)
 
 hold on;
 
-AUC = plotROCCurve(L3_L3Hat_MCINeg, L3_L3Hat_MCIPos, {'L3_L3Hat'}, 'L3_L3Hat_MCIposvsHCneg','MCIneg', 'MCIpos', config.color_scheme_npg(4,:), config);
-legendText = ['AUC(l3-$\hat{l3}$) = ' , num2str(round(AUC.Value(1),2),2)];
+AUC{1} = plotROCCurve(L3_L3Hat_MCINeg, L3_L3Hat_MCIPos, {'L3_L3Hat'}, 'L3_L3Hat_MCIposvsHCneg','MCIneg', 'MCIpos', config.color_scheme_npg(4,:), config);
+legendText{1,1} = ['AUC(l3-$\hat{l3}$) = ' , num2str(round(AUC{1}.Value(1),2),2)];
+
+AUC{2} = plotROCCurve(Theta3_Theta3Hat_MCINeg, Theta3_Theta3Hat_MCIPos, {'Theta3_Theta3Hat'}, 'Theta3_Theta3Hat_MCIvsHC','HC', 'MCI', config.color_scheme_npg(8,:), config);
+legendText{1,2} = ['AUC($\theta$3-$\hat{\theta 3}$) = ' , num2str(round(AUC{2}.Value(1),2),2)];
 
 hold off;
     
@@ -132,8 +137,8 @@ set(gca, ...
 
 axis square;
 
-exportgraphics(f,config.ResultFolder+"/ROCL3_L3Hat_MCIposvsneg"+".png",'Resolution',300);
-exportgraphics(f,config.ResultFolder+"/ROCL3_L3Hat_MCIposvsneg"+".pdf",'Resolution',300, 'ContentType','vector');
+exportgraphics(f,config.ResultFolder+"/ROC_ActualVsReal_MCIposvsneg"+".png",'Resolution',300);
+exportgraphics(f,config.ResultFolder+"/ROC_ActualVsReal_MCIposvsneg"+".pdf",'Resolution',300, 'ContentType','vector');
 
 clear parametersName filesName i colors f ll legendText 
 
@@ -141,33 +146,51 @@ clear parametersName filesName i colors f ll legendText
 % each value is a mean value for per participant
 % l3 is the real distance to the first cone (origin)
 % l3hat is the actual distance of the last segme
-function [l3_l3hat] = getReturnAndActualDistance(AllX, AllDX)
+function [l3_l3hat, theta3_theta3hat] = getReturnAndActualDistance(AllX, AllDX, AllTheta)
     
     nParticipants = length(AllX);
 
     %Averaged Across Trials
-    l3      = nan(1,nParticipants);
-    l3hat    = nan(1,nParticipants);
+    l3        = nan(1,nParticipants);
+    l3hat     = nan(1,nParticipants);
+    theta3    = nan(1,nParticipants); 
+    theta3hat = nan(1,nParticipants);
 
     for ip = 1:nParticipants
 
-        X = AllX{ip};
-        DX = AllDX{ip};
-        
-        currl3    = nan(1,length(X));
-        currl3hat = nan(1,length(X));
-        
+        X     = AllX{ip};
+        DX    = AllDX{ip};
+        Theta = AllTheta{ip};
+
+        currl3        = nan(1,length(X));
+        currl3hat     = nan(1,length(X));
+        currTheta3    = nan(1, length(Theta));
+        currTheta3hat = nan(1, length(Theta));
+
         for tr = 1:length(X)
-            currl3(tr)    = DX{tr}(3);
-            currl3hat(tr) = norm(X{tr}(3,:));
+
+            currl3(tr)        = DX{tr}(3);
+            currl3hat(tr)     = norm(X{tr}(3,:));
+            currTheta3(tr)    = Theta{tr}(3);
+            %correct return angle 
+            p1 = X{tr}(1,:);
+            p2 = X{tr}(2,:);
+            p3 = X{tr}(3,:);
+            vec1 = p3-p2; vec2 = p1-p3;
+            correct_theta3=atan2d(vec1(1)*vec2(2)-vec1(2)*vec2(1),vec1(1)*vec2(1)+vec1(2)*vec2(2));
+            currTheta3hat(tr) = deg2rad(correct_theta3);
+
         end
 
-        l3(ip)    = mean(currl3);
-        l3hat(ip) = mean(currl3hat);
+        l3(ip)        = mean(currl3);
+        l3hat(ip)     = mean(currl3hat);
+        theta3(ip)    = mean(currTheta3); 
+        theta3hat(ip) = mean(currTheta3hat);
 
     end
 
     l3_l3hat = l3' - l3hat';
+    theta3_theta3hat = theta3' - theta3hat';
 
 end
 

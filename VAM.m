4d53bd1df643eeace7046745826f5d8ba920bc52
Lenakeiz_UCL <andrea.castegnaro@outlook.com>
2022-06-24 17:24:20 +0100

    Added main entry point

--- VAM.m
+++ VAM.m
@@ -1,2 +1,8 @@
 %% Main entry point for the Vector Addiction Modelling (VAM) project
-% 
+% This script loads the data the from the Howett 2019., Brain paper. It
+% prepare the variables and runs the modelling described in the current
+% paper. Each figure will have a separate script that can be run after this
+% script has been correctly executed. For additional dependecies please
+% refer to the README file.
+
+%% Loading the data

37f15c2205c15c4099e539f7ff64f4801b9f593e
Lenakeiz_UCL <andrea.castegnaro@outlook.com>
2023-03-28 10:00:33 +0100

    Refactored vector addition model figure S5

--- Main/Fig_S5_Vector_Addition_Model.m
+++ Main/Fig_S5_Vector_Addition_Model.m
@@ -1,89 +1,82 @@
-%% test beta1 beta2 model
-%% Preparing the data
+%% Script to create output for Fig. S5 - comparisons between a vector addition model inspired by Harootonian et al., 2020
+% Zilong Ji, UCL, 2022 zilong.ji@ucl.ac.uk
+% Using a vector addition model inspired by Harootonian et al., 2020 we
+% show that elderly participants forget the first leg of the oubound path,
+% more than the second one. This is a feature that we implemented in our
+% linear and angular generative model. The model proposed here is a vector
+% addition version of the GLAMPI, featuring production error in linear and
+% angular returns.
+
+% Preparing the data
 VAM_PrepareBaseConfig;
 
-%% Preprocessing the data
+% Preprocessing the data
 VAM_PreprocessData;
 
-%% Preparing the data and Slecting the Model
-
-%choose a model from the model zoo
+% Model fitting
 config.ModelName        =   "beta1_beta2_g2_g3_sigma_nu";
 config.ParamName        =   ["beta1", "beta2", "g2", "g3", "sigma", "nu"];
-
-% config.ModelName        =   "beta1_beta2_sigma_nu";
-% config.ParamName        =   ["beta1", "beta2", "sigma", "nu"];
-
 config.NumParams        =   length(config.ParamName);
 
-% Run the model
 VAM;
 
-%% Model run completed, preparing the data for plotting figures
-config.ResultFolder     =   pwd + "/Output/ModelFigures/"+config.ModelName+"/Young_HealthyOld_MCINeg";
-%create storing folder for trajectory if not exist
+% Preparing the output
+config.ResultFolder     =   pwd + "/Output/FigS5/"+config.ModelName;
 if ~exist(config.ResultFolder, 'dir')
    mkdir(config.ResultFolder);
 end
 
-%% Generating color scheme
+% Generating color scheme for our paper
 ColorPattern; 
 
-%% Getting Information from results:
+% Collecting information from output
 AllYoungParams      =   YoungControls.Results.estimatedParams;
 AllHealthyOldParams =   HealthyControls.Results.estimatedParams;
 AllMCIPosParams     =   MCIPos.Results.estimatedParams;
 AllMCINegParams     =   MCINeg.Results.estimatedParams;
 AllMCIUnkParams     =   MCIUnk.Results.estimatedParams;
 
-%%
-[AllMCIParams, AllMCIParamsStatusIndex]= MergeMCI(AllMCIPosParams, AllMCINegParams, AllMCIUnkParams);
-
-%% BarScatter Plot for Young 
-BoxPlotOfBeta1Beta2(AllYoungParams, config, "Young");
-
-%% BarScatter Plot for HealthyOld 
+% BarScatter Plot for HealthyOld 
 BoxPlotOfBeta1Beta2(AllHealthyOldParams, config, "Elderly");
 
-%% BarScatter Plot for MCI 
+% BarScatter Plot for MCI 
 BoxPlotOfBeta1Beta2(AllMCIParams, config, "MCI");
 
 %% Box Plot Of Fitted Parameters by avearaging three paramters from three conditions into one mean value
 function BoxPlotOfBeta1Beta2(AllParams, config, name)
     
-    numConds = 3; %3 is the condition number
+    numConds = 3; % environmental conditions
     ParamName = config.ParamName;
 
-    ParamAllConds = []; %dimension are different, so separate from MCIParamAllConds
+    ParamAllConds = [];
     for TRIAL_FILTER=1:numConds
-        %% extract data
+        %% extract data for each condition
         Param     = AllParams{TRIAL_FILTER}(:,1);
         ParamAllConds = [ParamAllConds,Param];       
     end
 
-    %remove Nan rows (nan coz of 1, removing participants with short walking length; 2, not enough trials for parameter estimation)
+    % remove Nan rows (because of 1) removing participants with short walking length; 2) not enough trials for parameter estimation)
     ParamAllConds = removeNanRows(ParamAllConds);
 
     Beta1 = mean(ParamAllConds, 2);
 
-    ParamAllConds = []; %dimension are different, so separate from MCIParamAllConds
+    ParamAllConds = []; 
     for TRIAL_FILTER=1:numConds
-        %% extract data
+        %% extract data for each condition
         Param     = AllParams{TRIAL_FILTER}(:,2);
         ParamAllConds = [ParamAllConds,Param];       
     end
 
-    %remove Nan rows (nan coz of 1, removing participants with short walking length; 2, not enough trials for parameter estimation)
+    % remove Nan rows (because of 1) removing participants with short walking length; 2) not enough trials for parameter estimation)
     ParamAllConds = removeNanRows(ParamAllConds);
 
     Beta2 = mean(ParamAllConds, 2);
 
-    signrank(Beta1,Beta2,'tail','left')
+    % Calculating difference between groups
+    [h,pValue, stats] = ttest(Beta1, Beta2, "Tail", "left")
 
-    f = figure('visible','on','Position', [100 100, 400, 600]);
-    %%% Font type and size setting %%%
-    % Using Arial as default because all journals normally require the font to
-    % be either Arial or Helvetica
+    f = figure('visible','off','Position', [100 100, 400, 600]);
+
     set(0,'DefaultAxesFontName','Arial')
     set(0,'DefaultTextFontName','Arial')
     set(0,'DefaultAxesFontSize',12)
@@ -91,20 +84,19 @@ function BoxPlotOfBeta1Beta2(AllParams, config, name)
     
     Color1 = [0.5,0.5,0.5];
     Color2 = [0.5,0.5,0.5];
-    
-    
-    %bar scatter plot
+        
+    %% Scatter plot
     bar(1, mean(Beta1),'FaceColor', Color1, 'EdgeColor', 'k', 'BarWidth',0.4, 'FaceAlpha', 0.8);
     hold on
     bar(2, mean(Beta2),'FaceColor', Color2, 'EdgeColor', 'k', 'BarWidth',0.4, 'FaceAlpha', 0.8);
     
-    %add horizontal connection lines
+    %% Connecting points from same participant
     for i=1:length(Beta1)
         hold on
         plot([1,2], [Beta1(i), Beta2(i)], 'Color',[0,0,0,0.2], LineWidth=1);
     end
     
-    %scatter plot
+    %% Scatter plot
     hold on
     scatter(ones(length(Beta1),1), Beta1,100, 'MarkerEdgeColor','k', ...
         'MarkerFaceColor',Color1, 'MarkerEdgeAlpha',0.2, 'MarkerFaceAlpha',0.2)
@@ -112,19 +104,18 @@ function BoxPlotOfBeta1Beta2(AllParams, config, name)
     scatter(2*ones(length(Beta2),1), Beta2,100, 'MarkerEdgeColor','k', ...
         'MarkerFaceColor',Color2, 'MarkerEdgeAlpha',0.2, 'MarkerFaceAlpha',0.2)
     
-    %link the mean value
+    %% Connect mean values
     hold on
     plot([1,2], [mean(Beta1), mean(Beta2)], 'Color',[0,0,0,1.0], LineWidth=2);
     
     hold on
-    %add a reference line y=1
+    %% Reference line
     yl = yline(1);
     yl.Color = 'red';
     yl.LineWidth = 2;
     yl.LineStyle = '--';
-    
-    %[h,p, stats] = ttest(Beta1, Beta2, "Tail", "left")
-    signrank(Beta1, Beta2, "Tail", "left");
+
+    %% Figure post-processing
     set(gca, ...
         'Box'         , 'off'     , ...
         'TickDir'     , 'out'     , ...
@@ -137,11 +128,9 @@ function BoxPlotOfBeta1Beta2(AllParams, config, name)
         'YLim'        , [0,1.8],... 
         'LineWidth'   , 1.0        );
 
-    %add sigstar 
-    pvalue = signrank(Beta1, Beta2, "Tail", "left");
-    AllP = [pvalue];
+    %% Add significance bars
+    AllP = [pValue];
     Xval = [[1,2]];
-    %select those P value smaller than 0.05 (only add line when p<0.05)
     PsigInd = AllP<0.05;
     if sum(AllP<0.05)>0
         Xval_select = Xval(PsigInd,:);
@@ -150,13 +139,12 @@ function BoxPlotOfBeta1Beta2(AllParams, config, name)
         for i=1:sum(AllP<0.05)
             XXX{i} = Xval_select(i,:);
         end
-        H=sigstar(XXX,AllP_select);
+        H=adjustablesigstar(XXX,AllP_select);
     end
 
     yline(1,Color='r',LineStyle='--',LineWidth=2);
-    %text(1.05*xL(1),1.05*yL(2),"t("+num2str(stat.df)+")="+num2str(round(stat.tstat,2))+", p="+num2str(p), 'FontSize', 15)
 
-    %% save figure
-    exportgraphics(f,config.ResultFolder+"/ZMergeCondsBox_beta1beta2.png",'Resolution',300);
-    exportgraphics(f,config.ResultFolder+"/ZMergeCondsBox_beta1beta2.pdf",'Resolution',300, 'ContentType','vector');
+    %% Export figure
+    exportgraphics(f,config.ResultFolder+"/MergeCondsBox_beta1beta2.png",'Resolution',300);
+    exportgraphics(f,config.ResultFolder+"/MergeCondsBox_beta1beta2.pdf",'Resolution',300, 'ContentType','vector');
 end
